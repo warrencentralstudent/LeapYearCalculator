403645536e94edbcaff7f96c33d19b153e43b0c4
Davian Affognon <affogdav000@warren.k12.in.us>
2024-08-22 11:48:18 -0400

    leaping

--- src/LeapYearCalculator.java
+++ src/LeapYearCalculator.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class LeapYearCalculator {
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            return false;
+        }
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
